181321843717930b84f4ff47dfefecb0c509b4a0
karan <karanbarot33344@gmail.com>
2021-04-24 11:42:12 -0400

    mongo

--- connectDb.js
+++ connectDb.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const config= require('config');
-
+const uri = process.env.MONGODB_URI
 const connectDB= async()=>{
     try {
-        await mongoose.connect(config.get('MongoDB.connectionString'), {useNewUrlParser: true ,useUnifiedTopology: true })
+        await mongoose.connect(uri, {useNewUrlParser: true ,useUnifiedTopology: true })
         console.log('connected to database')
     } catch (err) {
         console.log('unable to connect')
