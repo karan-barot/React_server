5b9dfd47705ad3b3b909dc6dd72e5c2aeb43bb3a
karan <karanbarot33344@gmail.com>
2021-04-24 11:27:13 -0400

    adasdas

--- Config/connectDB.js
+++ Config/connectDB.js
@@ -4,6 +4,7 @@ const config= require('config');
 const connectDB= async()=>{
     try {
         await mongoose.connect(config.get('MongoDB.connectionString'), {useNewUrlParser: true ,useUnifiedTopology: true })
+        mongoose.set('bufferCommands', false);
         console.log('connected to database')
     } catch (err) {
         console.log('unable to connect')
