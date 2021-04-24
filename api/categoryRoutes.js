570d5717ea7065f0507712fd10cbb56ee2c921fc
karan <karanbarot33344@gmail.com>
2021-04-24 11:23:45 -0400

    asdddd

--- api/categoryRoutes.js
+++ api/categoryRoutes.js
@@ -13,7 +13,7 @@ router.get('/',async(req,res)=>{
         
         res.send(allcategories)
     } catch (err) {
-        return res.status(500).send('Server Error'+ "      Karan      "+err.json());
+        return res.status(500).send('Server Error'+ "      Karan      "+err);
     }
 });
 
