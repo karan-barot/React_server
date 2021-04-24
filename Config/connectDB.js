7373a1c72515f9755cffc1066422370713d82f74
karan <karanbarot33344@gmail.com>
2021-04-24 00:30:06 -0400

    env

--- Config/connectDB.js
+++ Config/connectDB.js
@@ -1,11 +1,10 @@
-import config from './Config/default.js';
 const mongoose = require('mongoose');
 const config= require('config');
-//const dbcon=config.get('mongoDBConnection')
+const dbcon=config.get('mongoDBConnection')
 
 const connectDB= async()=>{
     try {
-        await mongoose.connect(`${config.mongoDBConnection}`, {useNewUrlParser: true ,useUnifiedTopology: true })
+        await mongoose.connect(dbcon, {useNewUrlParser: true ,useUnifiedTopology: true })
         console.log('connected to database')
     } catch (err) {
         console.log('unable to connect')
